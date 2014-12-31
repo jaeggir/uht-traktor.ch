5d1127ea6a297b84597fac0082ed551a1c1dc539
Roger Jäggi <roger.jaeggi@avrios.com>
2014-12-31 15:05:48 +0100

    UserService should implement BaseService.

--- uht-rest/src/main/java/ch/uhttraktor/website/rest/service/UserService.java
+++ uht-rest/src/main/java/ch/uhttraktor/website/rest/service/UserService.java
@@ -1,6 +1,7 @@
 package ch.uhttraktor.website.rest.service;
 
 import ch.uhttraktor.website.domain.User;
+import ch.uhttraktor.website.persistence.BaseRepository;
 import ch.uhttraktor.website.persistence.UserRepository;
 import ch.uhttraktor.website.rest.security.SecurityUtils;
 import org.springframework.stereotype.Service;
@@ -9,10 +10,15 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.inject.Inject;
 
 @Service
-public class UserService {
+public class UserService extends BaseService<User> {
 
     @Inject
     private UserRepository userRepository;
+
+    @Override
+    protected BaseRepository<User> getRepository() {
+        return userRepository;
+    }
 
     @Transactional(readOnly = true)
     public User getCurrentUser() {
