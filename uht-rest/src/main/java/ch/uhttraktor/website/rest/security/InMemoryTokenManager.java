d9280c15c75b9786fd155e7e8ad428783a69af94
Roger Jäggi <roger.jaeggi@avrios.com>
2015-01-03 01:21:22 +0100

    Housekeeping for InMemoryTokenManager.

--- uht-rest/src/main/java/ch/uhttraktor/website/rest/security/InMemoryTokenManager.java
+++ uht-rest/src/main/java/ch/uhttraktor/website/rest/security/InMemoryTokenManager.java
@@ -1,5 +1,7 @@
 package ch.uhttraktor.website.rest.security;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.codec.Base64;
 
@@ -7,13 +9,20 @@ import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class InMemoryTokenManager {
 
-    private final Map<String, UserDetails> validTokens;
+    private static final long TOKEN_LIFETIME_IN_MS = 30 * 60 * 1000l; // 30 minutes
+
+    private static final long CLEANUP_INTERVAL_IN_MS = 30 * 60 * 1000l; // 30 minutes
+
+    private final Map<String, Tuple> validTokens;
 
     private final SecureRandom secureRandom;
+
+    private long lastCleanup = System.currentTimeMillis();
 
     public InMemoryTokenManager() {
         this.validTokens = new HashMap<>();
@@ -32,7 +41,7 @@ public class InMemoryTokenManager {
         } while (validTokens.containsKey(token));
 
         TokenInfo tokenInfo = new TokenInfo(token, userDetails);
-        validTokens.put(token, userDetails);
+        validTokens.put(token, new Tuple(tokenInfo, userDetails));
 
         return tokenInfo;
     }
@@ -44,14 +53,51 @@ public class InMemoryTokenManager {
     }
 
     public String removeToken(String token) {
-        UserDetails userDetails = validTokens.remove(token);
-        if (userDetails != null) {
-            return userDetails.getUsername();
+        Tuple tuple = validTokens.remove(token);
+        if (tuple != null) {
+            return tuple.getUserDetails().getUsername();
         }
         return null;
     }
 
+    private void considerHousekeeping() {
+        if (System.currentTimeMillis() - lastCleanup > CLEANUP_INTERVAL_IN_MS) {
+            new Thread(() -> {
+                Iterator<Map.Entry<String, Tuple>> iterator = validTokens.entrySet().iterator();
+                while (iterator.hasNext()) {
+                    long created = iterator.next().getValue().getTokenInfo().getCreated();
+                    if ((System.currentTimeMillis() - created) > TOKEN_LIFETIME_IN_MS) {
+                        iterator.remove();
+                    }
+                }
+            }).start();
+            lastCleanup = System.currentTimeMillis();
+        }
+    }
+
     public UserDetails getUserDetails(String token) {
-        return validTokens.get(token);
+
+        // do some housekeeping from time to time (in a separate thread)
+        considerHousekeeping();
+
+        Tuple tuple = validTokens.get(token);
+        if (tuple != null) {
+            // token still valid?
+            if ((System.currentTimeMillis() - tuple.getTokenInfo().getCreated()) > TOKEN_LIFETIME_IN_MS) {
+                validTokens.remove(token);
+                return null;
+            } else {
+                return tuple.getUserDetails();
+            }
+        } else {
+            return null;
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    private class Tuple {
+        private TokenInfo tokenInfo;
+        private UserDetails userDetails;
     }
 }
